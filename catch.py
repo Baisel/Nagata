9fd6f3f5f72314cdb0d5a7736b02f1e080b3dc91
Tenma Endou <hyper.tenmakun@docomo.ne.jp>
2018-01-19 14:14:17 +0900

    perfect flag proccessing

--- catch.py
+++ catch.py
@@ -1,12 +1,12 @@
 import cgi
 from controllers.controller import defaultPosition, dragMove
+import sys
 
 
-with open('flag.txt', 'r') as f:
+with open('flag.txt', 'r') as f: # if flag is True,move cursor.
     flag = f.readline()
-    if flag == 'start':
+    if flag == 'True':
         form = cgi.FieldStorage()
-        count = int(form['count'].value)
+        args = sys.argv
         defaultPosition()
-        dragMove(count)
-
+        dragMove(int(args[1]))
